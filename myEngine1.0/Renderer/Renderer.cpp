02532b74909272a4910583714717c855310af2e8
Lucas Martin <Ayagami@users.noreply.github.com>
2014-11-25 12:06:53 +0000

    Improved some things.

--- myEngine1.0/Renderer/Renderer.cpp
+++ myEngine1.0/Renderer/Renderer.cpp
@@ -65,7 +65,7 @@ bool Renderer::Init(HWND _HwnD){
 	d3dpp.EnableAutoDepthStencil = TRUE;
 	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 	d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;
-	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;  // HAGAMOS 800 FRAMES, PORQUE PODEMOS.
+	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;  // HAGAMOS 2500 FRAMES, PORQUE PODEMOS.
 
 	if(d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, _HwnD, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &d3d_dev) == D3D_OK){
 		d3d_dev->SetRenderState(D3DRS_LIGHTING,FALSE);
